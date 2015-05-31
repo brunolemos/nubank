476c1e17b56192fbefd6e3a2a45663e26c8212c1
Bruno Lemos <brunohplemos@gmail.com>
2015-05-30 22:58:51 -0300

    Static Bills Page

--- Nubank/Nubank.Shared/App.xaml.cs
+++ Nubank/Nubank.Shared/App.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
 namespace Nubank
 {
@@ -95,7 +83,7 @@ namespace Nubank
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(BillsPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
