898eb76202c59547572f96159700bb3a7ebce148
Bruno Lemos <brunohplemos@gmail.com>
2015-05-31 00:03:24 -0300

    Better Experience With FlipView Instead Of Pivot

---
+++ Nubank/Nubank.WindowsPhone/Controls/ImprovedFlipView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Nubank.Controls
+{
+    public class ImprovedFlipView : FlipView
+    {
+        public ScrollViewer ScrollViewer { get; private set; }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.ScrollViewer = (ScrollViewer)this.GetTemplateChild("ScrollingHost");
+            this.ScrollViewer.ViewChanged += ScrollViewer_ViewChanged;
+        }
+
+        void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            int index = (int)Math.Round(this.ScrollViewer.HorizontalOffset - 2);
+            if (this.SelectedIndex != index) this.SelectedIndex = index;
+        }
+    }
+}
