a6e5119bf6d3fb38989e07cad2e3d2ffbe447981
Bruno Lemos <brunohplemos@gmail.com>
2015-06-06 17:11:37 -0300

    Fixed Pivot Header Opacity + Open Sans Font

---
+++ Nubank/Nubank.Shared/Controls/FixedPivot.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace Nubank.Controls
+{
+    public class FixedPivot : Pivot
+    {
+        protected PivotHeaderPanel PivotHeaderPanel { get; private set; }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+#if WINDOWS_PHONE_APP
+            this.PivotHeaderPanel = (PivotHeaderPanel)this.GetTemplateChild("Header");
+#else
+            this.PivotHeaderPanel = (PivotHeaderPanel)this.GetTemplateChild("StaticHeader");
+#endif
+
+            this.SelectionChanged += (s, e) => UpdateHeaderItemsOpacity();
+            this.Loaded += (s, e) => UpdateHeaderItemsOpacity();
+        }
+
+        private void UpdateHeaderItemsOpacity()
+        {
+            if (PivotHeaderPanel == null) return;
+
+            for (int i = 0; i < PivotHeaderPanel.Children.Count; i++)
+                (PivotHeaderPanel.Children[i] as PivotHeaderItem).ContentTemplateRoot.Opacity = this.SelectedIndex == i ? 1 : 0.4;
+        }
+    }
+}
