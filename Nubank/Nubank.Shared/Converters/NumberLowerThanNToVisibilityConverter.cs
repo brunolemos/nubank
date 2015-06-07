04a60e81d0fdf553f5d7d1ea2df554c29e098c5f
Bruno Lemos <brunohplemos@gmail.com>
2015-06-07 02:14:22 -0300

    Only Show Some Data When Positive/Negative

---
+++ Nubank/Nubank.Shared/Converters/NumberLowerThanNToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Nubank.Converters
+{
+    public sealed class NumberLowerThanNToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double a, b;
+            double.TryParse(value.ToString(), out a);
+            double.TryParse(parameter.ToString(), out b);
+
+            return a < b ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
